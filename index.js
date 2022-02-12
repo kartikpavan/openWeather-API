5a6ab1668eb1a6361eb74cf2569940c10bf1d5a8
kartikpavan <kartik.varanasi@yahoo.com>
2022-02-12 18:33:24 +0530

    API proxy server with rate limit and apicache

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const path = require("path");
+const app = express();
+const rateLimit = require("express-rate-limit");
+const apicache = require("apicache");
+
+//enable cors
+//! Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources on a web page
+//! to be requested from another domain outside the domain from which the first resource was served.
+app.use(cors());
+
+// RATE LIMITING
+//! Basic rate-limiting middleware for Express.
+//! Use to limit repeated requests to public APIs and/or endpoints such as password reset.
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, //? 15 minutes
+  max: 5, //? Limit each IP to 5 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, //? Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, //? Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+app.set("trust proxy", 1);
+//Routes
+app.use("/api", require("./routes/index"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (req, res) => {
+  console.log(`Serving on PORT ${PORT}`);
+});
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const needle = require("needle");
+const url = require("url");
+
+const apicache = require("apicache");
+
+//environment variables
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+//!API cache
+let cache = apicache.middleware;
+
+router.get("/", cache("2 minutes"), async (req, res) => {
+  try {
+    const params = new URLSearchParams({
+      [API_KEY_NAME]: API_KEY_VALUE,
+      ...url.parse(req.url, true).query,
+    });
+    const response = await needle("get", `${API_BASE_URL}?${params}`);
+    const data = response.body;
+
+    //log the request to the PUBLIC API
+    if (process.env.NODE_ENV !== "production") {
+      console.log(`request: ${API_BASE_URL}?${params}`);
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
+
+module.exports = router;
